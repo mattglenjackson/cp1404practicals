e23b94eaa88199380448ae0dd6d9a90ecd017294
mattglenjackson <matthew.jackson3@my.jcu.edu.au>
2022-09-23 12:57:49 +1000

    Refactor scores program to use functions

--- Prac_02/scores.py
+++ Prac_02/scores.py
@@ -3,12 +3,24 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
+
+def main():
+    """Main function for scores.py."""
+    score = get_score()
+    if score < 0 or score > 100:
+        print("Invalid score")
+    elif score >= 90:
         print("Excellent")
-elif score >= 50:
+    elif score >= 50:
         print("Passable")
-else:
-    print("Bad")
+    else:
+        print("Bad")
+
+
+def get_score():
+    """Get score from user."""
+    score = float(input("Enter score: "))
+    return score
+
+
+main()
