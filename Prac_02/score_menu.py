58123a1c9bc9807ad3a1dabba62860e49b1907f8
mattglenjackson <matthew.jackson3@my.jcu.edu.au>
2022-09-25 14:36:54 +1000

    Add score menu program

---
+++ Prac_02/score_menu.py
@@ -0,0 +1,100 @@
+"""
+MENU = (E)nter score (P)rint result Print (S)tar (Q)uit
+
+function main
+    score = -1
+    print MENU
+    get choice
+    while choice != "Q"
+        if choice = "E"
+            score = call get_valid_score function
+        else if choice = "P"
+            grade = call determine_grade function
+            print grade
+        else if choice = "S"
+            call print_stars function
+        else
+            print "Invalid input"
+
+        print MENU
+        get choice
+
+    print farewell message
+
+
+function get_valid_score
+    get score
+    while score < 0 or score > 100
+        print "Invalid score"
+        get score
+    return score
+
+
+function determine_grade(score):
+    if score = -1
+        grade = "Please enter a score"
+    elif score >= 90
+        grade = "Excellent"
+    elif score >= 50
+        grade = "Passable"
+    else
+        grade = "Bad"
+    return grade
+
+
+print_stars(score)
+    print "*" score times
+
+
+call main
+"""
+
+MENU = "(E)nter score\n(P)rint result\nPrint (S)tar\n(Q)uit"
+
+
+def main():
+    score = -1
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "E":
+            score = get_valid_score()
+        elif choice == "P":
+            grade = determine_grade(score)
+            print(grade)
+        elif choice == "S":
+            print_stars(score)
+        else:
+            print("Invalid input")
+
+        print(MENU)
+        choice = input(">>> ").upper()
+
+    print("Farewell")
+
+
+def get_valid_score():
+    score = int(input("Enter Score: "))
+    while score < 0 or score > 100:
+        print("Invalid score")
+        score = int(input("Enter Score: "))
+    return score
+
+
+def determine_grade(score):
+    if score == -1:
+        grade = "Please enter a score"
+    elif score >= 90:
+        grade = "Excellent"
+    elif score >= 50:
+        grade = "Passable"
+    else:
+        grade = "Bad"
+    return grade
+
+
+def print_stars(score):
+    print("*" * score)
+
+
+main()
