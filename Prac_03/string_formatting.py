ed3be8ababab290e69d32f22293908e1a7c51b2e
mattglenjackson <matthew.jackson3@my.jcu.edu.au>
2022-09-30 20:55:40 +1000

    Changed string formatting program to use f-string and string formatting as per program requirement

--- Prac_03/string_formatting.py
+++ Prac_03/string_formatting.py
@@ -32,10 +32,14 @@ numbers = [1, 19, 123, 456, -25]
 for i, number in enumerate(numbers, 1):
     print(f"Number {i} is {number:5}")
 
-# TODO: Use f-string formatting to produce the output:
+print(f"{year} {name} for about ${cost:.0f}")
 # 1922 Gibson L-5 CES for about $16,035!
 
-# TODO: Using a for loop with the range function and string formatting,
+for i in range(0, 151, 50):
+    print(f"{i:>3}")
+
+for i in range(0, 151, 50):
+    print("{:>3}".format(i))
 # produce the following right-aligned output (DO NOT use a list):
 #   0
 #  50
