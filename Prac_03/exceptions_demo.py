8e0c6ead7d10592cf6c264b59d5f87c6e3bd13fb
mattglenjackson <matthew.jackson3@my.jcu.edu.au>
2022-10-01 18:16:04 +1000

    Answered questions in exceptions demo program

--- Prac_03/exceptions_demo.py
+++ Prac_03/exceptions_demo.py
@@ -1,9 +1,9 @@
 """
 CP1404/CP5632 - Practical
 Answer the following questions:
-1. When will a ValueError occur?
-2. When will a ZeroDivisionError occur?
-3. Could you change the code to avoid the possibility of a ZeroDivisionError?
+1. When will a ValueError occur? When the numerator or denominator is anything other than an integer
+2. When will a ZeroDivisionError occur? When the denominator is equal to 0
+3. Could you change the code to avoid the possibility of a ZeroDivisionError? Yes i would use a standard error checking while loop that would prevent denominator for being 0
 """
 
 try:
