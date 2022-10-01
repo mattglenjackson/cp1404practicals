6d879941a0455de730f85a0836c1f64a4bd4afa2
mattglenjackson <matthew.jackson3@my.jcu.edu.au>
2022-10-01 18:27:35 +1000

    Improved exceptions demo program by adding while loop that prevents denominator from being equal to 0, Added additional comments.

--- Prac_03/exceptions_demo.py
+++ Prac_03/exceptions_demo.py
@@ -9,10 +9,13 @@ Answer the following questions:
 try:
     numerator = int(input("Enter the numerator: "))
     denominator = int(input("Enter the denominator: "))
+    while denominator == 0:
+        print("Denominator cannot equal 0")
+        denominator = int(input("Enter the denominator: "))
     fraction = numerator / denominator
     print(fraction)
 except ValueError:
     print("Numerator and denominator must be valid numbers!")
-except ZeroDivisionError:
-    print("Cannot divide by zero!")
+# except ZeroDivisionError: # Redundant now because of while loop
+#     print("Cannot divide by zero!")
 print("Finished.")
