dbeb886b7593f1599e28bf175a0e6f31ae2fc389
mattglenjackson <matthew.jackson3@my.jcu.edu.au>
2022-10-11 21:56:27 +1000

    Changed variable and constant names in quick picks program as original names were too similar and refactored pseudocode

--- Prac_04/quick_picks.py
+++ Prac_04/quick_picks.py
@@ -1,19 +1,35 @@
 """
 MIN_NUMBER = 1
 MAX_NUMBER = 45
-NUMBERS_PER_QUICK_PICK
+AMOUNT_OF_NUMBERS_PER_QUICK_PICK = 6
 
 get number_of_quick_picks
 
 for i in number_of_quick_picks
-    quick_pick_numbers = []
-    for j in range NUMBERS_PER_QUICK_PICK
+    numbers = []
+    for j in range AMOUNT_OF_NUMBERS_PER_QUICK_PICK
         number = random integer between MIN_NUMBER and MAX_NUMBER
-        while number is in quick_pick_numbers
+        while number is in numbers
             number = random integer between MIN_NUMBER and MAX_NUMBER
-        append number to quick_pick_numbers
-        sort quick_pick_numbers in numerical order
-    print quick_pick_numbers in message with spacing formatting
-
+        append number to numbers
+        sort numbers in numerical order
+    print numbers in message with spacing formatting
 """
 
+from random import randint
+
+MIN_NUMBER = 1
+MAX_NUMBER = 45
+AMOUNT_OF_NUMBERS_PER_QUICK_PICK = 6
+
+number_of_quick_picks = int(input("How many quick picks? "))
+
+for i in range(number_of_quick_picks):
+    numbers = []
+    for j in range(AMOUNT_OF_NUMBERS_PER_QUICK_PICK):
+        number = randint(MIN_NUMBER, MAX_NUMBER)
+        while number in numbers:
+            number = randint(MIN_NUMBER, MAX_NUMBER)
+        numbers.append(number)
+        numbers.sort()
+    print("{:2} {:2} {:2} {:2} {:2} {:2}".format(*numbers))
