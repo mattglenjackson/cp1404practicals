3d441a5b766b34e081b34d18759578067335564b
mattglenjackson <matthew.jackson3@my.jcu.edu.au>
2022-10-11 20:24:22 +1000

    Changed list exercises program to use for loop to process output as per requirements

--- Prac_04/list_exercises.py
+++ Prac_04/list_exercises.py
@@ -12,3 +12,14 @@ Sample output of program
    The average of the numbers is 6.2
 """
 
+numbers = []
+
+for i in range(5):
+    number = int(input("Number: "))
+    numbers.append(number)
+
+print(f"The first number is {numbers[0]}")
+print(f"The last number is {numbers[-1]}")
+print(f"The smallest number is {min(numbers)}")
+print(f"The largest number is {max(numbers)}")
+print(f"The average of numbers is {sum(numbers) / len(numbers)}")
