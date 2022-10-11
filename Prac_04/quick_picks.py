8624c9c14a83c3c52127006cefa8d2d0c450eadb
mattglenjackson <matthew.jackson3@my.jcu.edu.au>
2022-10-11 21:44:32 +1000

    Added quick picks program with pseudocode

---
+++ Prac_04/quick_picks.py
@@ -0,0 +1,19 @@
+"""
+MIN_NUMBER = 1
+MAX_NUMBER = 45
+NUMBERS_PER_QUICK_PICK
+
+get number_of_quick_picks
+
+for i in number_of_quick_picks
+    quick_pick_numbers = []
+    for j in range NUMBERS_PER_QUICK_PICK
+        number = random integer between MIN_NUMBER and MAX_NUMBER
+        while number is in quick_pick_numbers
+            number = random integer between MIN_NUMBER and MAX_NUMBER
+        append number to quick_pick_numbers
+        sort quick_pick_numbers in numerical order
+    print quick_pick_numbers in message with spacing formatting
+
+"""
+
