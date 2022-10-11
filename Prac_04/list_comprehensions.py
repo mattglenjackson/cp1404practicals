c34269d480eb9ad619ae1b6ca87ef0338e0cbb34
mattglenjackson <matthew.jackson3@my.jcu.edu.au>
2022-10-11 21:02:05 +1000

    Created variables in list comprehension program to be assigned before printing like the examples

--- Prac_04/list_comprehensions.py
+++ Prac_04/list_comprehensions.py
@@ -35,15 +35,17 @@ print(lowercase_full_names)
 
 almost_numbers = ['0', '10', '21', '3', '-7', '88', '9']
 # TODO: list comprehension to create a list of integers from the above list of strings
-numbers = [int(number) for number in almost_numbers]
+numbers = [int(almost_number) for almost_number in almost_numbers]
 print(numbers)
 
 # TODO: list comprehension to create a list of only the numbers that are
 #  greater than 9 from the numbers (not strings) you just created
-print([number for number in numbers if number > 9])
+large_numbers = ([number for number in numbers if number > 9])
+print(large_numbers)
 
 # TODO: (more advanced) use a list comprehension and the join string method
 # to create a string (not list) of the last names for those full names longer than 11 characters
 # the result should be: 'Harlem, Hendrix, Lovelace'
 
-print(",".join([(name[name.find(" "):]) for name in full_names if len(name) > 11]))
+names_string = ",".join([(name[name.find(" "):]) for name in full_names if len(name) > 11])
+print(names_string)
