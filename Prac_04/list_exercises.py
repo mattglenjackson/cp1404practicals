de1cca5d1c5e25830db5f1b713ad88be9890727a
mattglenjackson <matthew.jackson3@my.jcu.edu.au>
2022-10-11 20:19:52 +1000

    Added list exercises program

---
+++ Prac_04/list_exercises.py
@@ -0,0 +1,14 @@
+"""
+Sample output of program
+   Number: 5
+   Number: 20
+   Number: 1
+   Number: 2
+   Number: 3
+   The first number is 5
+   The last number is 3
+   The smallest number is 1
+   The largest number is 20
+   The average of the numbers is 6.2
+"""
+
