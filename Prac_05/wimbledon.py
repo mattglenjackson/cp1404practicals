69c2ca01e315928fa941aeb611e254966ebda705
mattglenjackson <matthew.jackson3@my.jcu.edu.au>
2022-10-18 17:24:46 +1000

    Completed wimbledon program as per prac requirements

--- Prac_05/wimbledon.py
+++ Prac_05/wimbledon.py
@@ -2,11 +2,14 @@ FILENAME = "wimbledon.csv"
 
 
 def main():
+    """Wimbledon.py main function"""
     records = get_records(FILENAME)
     countries, champion_to_times_won = process_data(records)
+    print_results(countries, champion_to_times_won)
 
 
 def get_records(filename):
+    """Gets record from file."""
     records = []
     with open(filename, "r", encoding="utf-8-sig") as in_file:
         in_file.readline()  # Remove header from columns
@@ -17,6 +20,7 @@ def get_records(filename):
 
 
 def process_data(records):
+    """Processes data into dictionary and set"""
     countries = set()
     champion_to_times_won = {}
     count = 0
@@ -31,6 +35,16 @@ def process_data(records):
             champion_to_times_won[champion] = count
 
     return countries, champion_to_times_won
+
+
+def print_results(countries, champion_to_times_won):
+    """Prints results of champions and winning countries of Wimbledon."""
+    print("Wimbledon Champions:")
+    for champion in champion_to_times_won:
+        print(f"{champion} {champion_to_times_won[champion]}")
+    print()
+    print(f"These {len(countries)} countries have won Wimbledon:")
+    print(",".join(country for country in sorted(countries)))
 
 
 if __name__ == '__main__':
