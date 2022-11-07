6ca2e76d2b9f8d34c089607f6f30083679ae7358
mattglenjackson <matthew.jackson3@my.jcu.edu.au>
2022-11-07 20:44:57 +1000

    Refactored menu code into main function; refactored add_guitar and save_guitars into myguitars program

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -6,9 +6,23 @@ MENU = "(D)isplay Guitars\n(A)dd Guitar\n(Q)uit"
 
 def main():
     """Loads guitars file guitar file, sorts guitars and then displays them."""
+    print("Welcome to my guitar program for CP1404.")
     guitars = load_guitars(FILENAME)
     sort_guitars(guitars)
-    display_guitars(guitars)
+    print(MENU)
+    choice = input('>>> ').upper()
+    while choice != "Q":
+        if choice == "D":
+            display_guitars(guitars)
+        elif choice == "A":
+            add_guitar(guitars)
+            sort_guitars(guitars)
+        else:
+            print("Invalid choice.")
+        print(MENU)
+        choice = input('>>> ').upper()
+    save_guitars(FILENAME, guitars)
+    print("Farewell!")
 
 
 def load_guitars(filename):
@@ -29,9 +43,26 @@ def display_guitars(guitars):
         print(guitar)
 
 
+def add_guitar(guitars):
+    """Add guitar to list of guitars"""
+    name = input("Name: ")
+    year = int(input("Year: "))
+    cost = float(input("Cost $: "))
+    new_guitar = Guitar(name, year, cost)
+    guitars.append(new_guitar)
+
+
 def sort_guitars(guitars):
     """Sort list of guitars."""
     guitars.sort()
+
+
+def save_guitars(filename, guitars):
+    """Save list of guitars to guitars file."""
+    out_file = open(filename, 'w')
+    for guitar in guitars:
+        print(f"{guitar.name},{guitar.year},{guitar.cost}", file=out_file)
+    out_file.close()
 
 
 main()
